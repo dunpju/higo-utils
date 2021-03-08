8fc93a163ce1150ec4113d9dc1b1e34726acf176
dengpengju <dengpju@163.com>
2021-03-08 22:03:31 +0800

    封装map操作

--- test/main.go
+++ test/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/dengpju/higo-utils/utils"
 )
 
-func main()  {
-	fmt.Println(utils.Dir("./utils").Scan().Suffix("go").List())
+func main() {
+	fmt.Println(utils.Dir("./utils").Scan().Suffix("go").Get())
+	result := utils.MapOperation(make(map[string]interface{})).Append("key1", "value1").Append("key2", "value2")
+	fmt.Println(result)
 }
--- utils/MapUtil.go
+++ utils/MapUtil.go
@@ -13,6 +13,24 @@ func NewMap(map1 map[string]interface{}, map2 map[string]interface{}) *Map {
 
 // 合并map2到map1
 func (this *Map) Merge() *map[string]interface{} {
-	for k ,v := range this.Map1 { this.Map2[k] = v }
+	for k, v := range this.Map1 {
+		this.Map2[k] = v
+	}
 	return &this.Map1
+}
+
+type MapString map[string]interface{}
+
+func MapOperation(m MapString) MapString {
+	return m
+}
+
+func (this MapString) Append(key string, value interface{}) MapString {
+	this[key] = value
+	return this
+}
+
+func MapAppend(m map[string]interface{}, key string, value interface{}) map[string]interface{} {
+	m[key] = value
+	return m
 }
