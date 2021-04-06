9e9f802a1927686095c29bf971fbaf357cb0a42c
dengpengju <dengpju@163.com>
2021-04-06 19:34:00 +0800

    封装base64编码

---
+++ utils/Encode.go
@@ -0,0 +1,16 @@
+package utils
+
+import "encoding/base64"
+
+func Base64Decode(src string) []byte {
+	b, err := base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func Base64Encode(src []byte) string {
+	s := base64.StdEncoding.EncodeToString(src)
+	return s
+}
