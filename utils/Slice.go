6f1c6f00fc0a7e0c6b6a42f97d2e6a62025dbb1e
dengpengju <dengpju@163.com>
2021-04-19 10:22:10 +0800

    更新

--- utils/Slice.go
+++ utils/Slice.go
@@ -1,5 +1,17 @@
 package utils
 
+import "strings"
+
+type SliceString []string
+
+func (this SliceString) String(sep ...string) string {
+	s := ""
+	if len(sep) > 0 {
+		s = sep[0]
+	}
+	return strings.Join(this, s)
+}
+
 //byte切片倒序
 func ByteReverse(s []byte) []byte {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
