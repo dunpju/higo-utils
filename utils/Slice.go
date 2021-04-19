87c277a201d668e29132003a199fc67fcea62347
dengpengju <dengpju@163.com>
2021-04-19 10:54:07 +0800

    封装切片操作

--- utils/Slice.go
+++ utils/Slice.go
@@ -12,6 +12,46 @@ func (this SliceString) String(sep ...string) string {
 	return strings.Join(this, s)
 }
 
+func (this SliceString) Append(value string) SliceString {
+	this = append(this, value)
+	return this
+}
+
+func (this SliceString) Remove(dist string) SliceString {
+	var tmp = SliceString{}
+	this.ForEach(func(index int, value string) {
+		if value != dist {
+			tmp.Append(value)
+		}
+	})
+	return tmp
+}
+
+func (this SliceString) Replace(src, dist string) SliceString {
+	this.ForEach(func(index int, value string) {
+		if value == src {
+			this[index] = dist
+		}
+	})
+	return this
+}
+
+func (this SliceString) Empty() {
+	this = nil
+}
+
+func (this SliceString) Len() int {
+	return len(this)
+}
+
+type SliceStringCallable func(index int, value string)
+
+func (this SliceString) ForEach(callable SliceStringCallable) {
+	for key, value := range this {
+		callable(key, value)
+	}
+}
+
 //byte切片倒序
 func ByteReverse(s []byte) []byte {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
