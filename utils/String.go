b212db5dd83e2df54e679d44d06043fb60fb8014
dengpju <dengpju@163.com>
2021-08-25 18:49:23 +0800

    更新

--- utils/String.go
+++ utils/String.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"bytes"
+	"log"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -39,6 +42,7 @@ func Int64String(i int64) string {
 	return strconv.FormatInt(i, 10)
 }
 
+// 首字母大写
 func Ucfirst(str string) string {
 	for i, v := range str {
 		return string(unicode.ToUpper(v)) + str[i+1:]
@@ -46,9 +50,81 @@ func Ucfirst(str string) string {
 	return ""
 }
 
+// 首字母小写
 func Lcfirst(str string) string {
 	for i, v := range str {
 		return string(unicode.ToLower(v)) + str[i+1:]
 	}
 	return ""
+}
+
+// 驼峰式写法转为下划线写法
+func CamelToCase(name string) string {
+	buffer := NewBuffer()
+	for i, r := range name {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				buffer.Append('_')
+			}
+			buffer.Append(unicode.ToLower(r))
+		} else {
+			buffer.Append(r)
+		}
+	}
+	return buffer.String()
+}
+
+// 下划线写法转为驼峰写法
+func CaseToCamel(name string) string {
+	name = strings.Replace(name, "_", " ", -1)
+	name = strings.Title(name)
+	return strings.Replace(name, " ", "", -1)
+}
+
+// 内嵌bytes.Buffer，支持连写
+type Buffer struct {
+	*bytes.Buffer
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{Buffer: new(bytes.Buffer)}
+}
+
+func (b *Buffer) Append(i interface{}) *Buffer {
+	switch val := i.(type) {
+	case int:
+		b.append(strconv.Itoa(val))
+	case int64:
+		b.append(strconv.FormatInt(val, 10))
+	case uint:
+		b.append(strconv.FormatUint(uint64(val), 10))
+	case uint64:
+		b.append(strconv.FormatUint(val, 10))
+	case string:
+		b.append(val)
+	case []byte:
+		_, err := b.Write(val)
+		if err != nil {
+			panic(err)
+		}
+	case rune:
+		_, err := b.WriteRune(val)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return b
+}
+
+func (b *Buffer) append(s string) *Buffer {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("*****内存不够了！******")
+		}
+	}()
+	_, err := b.WriteString(s)
+	if err != nil {
+		panic(err)
+	}
+	return b
 }
