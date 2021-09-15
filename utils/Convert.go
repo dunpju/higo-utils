c127a3158032b9cc017bd4b79e3807e91c2ad5a4
dengpju <dengpju@163.com>
2021-09-15 16:18:19 +0800

    更新

--- utils/Convert.go
+++ utils/Convert.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 //转换为字符串切片
@@ -28,4 +30,28 @@ func ToJson(struc interface{}) string {
 		panic(err)
 	}
 	return string(mjson)
+}
+
+//json key 转小驼峰
+func JsonKeyToLcCamel(str string) string {
+	re, err := regexp.Compile(`("[_a-zA-Z]*?":)`)
+	if nil != err {
+		panic(err)
+	}
+	pregReplace := re.ReplaceAllFunc([]byte(str), func(bytes []byte) []byte {
+		return []byte(`"` + Lcfirst(strings.Replace(CaseToCamel(string(bytes)), `"`, "", 1)))
+	})
+	return string(pregReplace)
+}
+
+//json key 转大驼峰
+func JsonKeyToCamel(str string) string {
+	re, err := regexp.Compile(`("[_a-zA-Z]*?":)`)
+	if nil != err {
+		panic(err)
+	}
+	pregReplace := re.ReplaceAllFunc([]byte(str), func(bytes []byte) []byte {
+		return []byte(CaseToCamel(string(bytes)))
+	})
+	return string(pregReplace)
 }
